f2fe35b5af025686fe706b19ee36dab10f9030ad
Marwan Musa <marwanlmusa@gmail.com>
2023-01-31 11:35:02 +0700

    add PredictionSerializer

--- monitor/serializers.py
+++ monitor/serializers.py
@@ -1,4 +1,13 @@
 from rest_framework import serializers
+from .models import *
 
-class ModelPredictionSerilizer(serializers.Srializer):
+class PredictionSerializer(serializers.ModelSerializer):
+    age = serializers.IntegerField()
+    gender = serializers.ChoiceField(Prediction.group_sex, required=True)
+    bp = serializers.ChoiceField(Prediction.group_bp, required=True)
+    cholesterol = serializers.ChoiceField(Prediction.group_chol, required=True)
+    salt = serializers.FloatField()
     
+    class Meta:
+        model = Prediction
+        fields = '__all__'
