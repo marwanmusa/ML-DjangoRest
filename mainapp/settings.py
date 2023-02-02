000fdf139d32910c98a920e61f74d697973738d0
Marwan Musa <marwanlmusa@gmail.com>
2023-02-02 15:18:24 +0700

    add dotenv file to store important keys

--- mainapp/settings.py
+++ mainapp/settings.py
@@ -10,7 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
+from dotenv import load_dotenv
 from pathlib import Path
+
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+load_dotenv(dotenv_path)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tc!6=9f8f^-rpxv%6m8jb#ydpkn6^=muh3g+p2a)qp$hb7%a##'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- ml_api/urls.py
+++ ml_api/urls.py
@@ -3,6 +3,6 @@ from rest_framework import routers
 from .views import *
 
 router = routers.DefaultRouter()
-router.register(prefix='prediction', viewset = WeightPredictionViewSet)
+router.register(prefix='weight-prediction', viewset = WeightPredictionViewSet)
 
 urlpatterns = router.urls
